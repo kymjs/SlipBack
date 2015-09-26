3b95a2c858eddc7f22a4e79f3ed4697c39ceda65
kymjs <kymjs123@gmail.com>
2015-09-26 20:59:03 +0800

    first

---
+++ SwipleBack/app/src/main/java/com/kymjs/swipleback/MainActivity.java
@@ -0,0 +1,20 @@
+package com.kymjs.swipleback;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends SwipeBackActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+            }
+        });
+    }
+}
